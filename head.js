a6c9c8f90df812cbd522e4d2ac3fcad61ccb0999
BigBadAI <flashmefusion@hotmail.ca>
2021-01-04 23:25:42 +0000

    head funtion for selecting item 0 in an array

---
+++ head.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚫🚫🚫 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const head = function(array) {
+  return array[0];
+};
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([5,7,8]), 2);
+assertEqual(head([]), 5);
