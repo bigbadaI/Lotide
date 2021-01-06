ff1125a7e42167a338c31e1d55e17be685ebbd01
BigBadAI <flashmefusion@hotmail.ca>
2021-01-06 01:20:13 +0000

    had to omit out the testing material I had at the bottom

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -10,11 +10,10 @@ const assertArraysEqual = function(actual, expected) {
   
   }));
   console.log(result);
-  //return true outcome for the assertEqual function
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+/*assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
 assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
 
 assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false*/
