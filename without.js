3894d5b37d6f11f48c0b50fb0d24686f70191c5c
BigBadAI <flashmefusion@hotmail.ca>
2021-01-06 02:18:13 +0000

    a function to take in two arrays and omit anything from the first array that matches the second array

---
+++ without.js
@@ -0,0 +1,23 @@
+const assertArraysEqual = function(actual, expected) {
+  let result = '';
+  actual.forEach((example1) => expected.forEach(example2 => {
+     
+    if (example1 !== example2) {
+      result = `🚫🚫🚫 Assertion Failed: ${actual} !== ${expected}`;
+    } else {
+      result = `👍👍👍 Assertion Passed: ${actual} === ${expected}`;
+    }
+  
+  }));
+  console.log(result);
+};
+const without = function(originalArray, erase) {
+  let withoutArray = originalArray.filter((item) => !erase.includes(item));
+  return withoutArray;
+};
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+without([1, 2, 3], [1]); // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
