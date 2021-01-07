60e692203338cc0456ef8b220bc526e136381cca
BigBadAI <flashmefusion@hotmail.ca>
2021-01-07 02:15:53 +0000

    a function to take a given value and run through the keys of an object and return the given key

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚫🚫🚫 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  // for of loop to check each key of the given object
+  for (let property of Object.keys(object)) {
+    //if statement to check the object key against the given value
+    if (object[property] === value) {
+      return property;
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
