afe746b4ecbf386bf2aaa81ff6135809eea6c7a4
BigBadAI <flashmefusion@hotmail.ca>
2021-01-07 05:15:26 +0000

    updated code to omit some unessecary parts after use in other functions noticed it wasn't need or doing anything

--- eqArrays.js
+++ eqArrays.js
@@ -8,20 +8,18 @@ const assertEqual = function(actual, expected) {
 const eqArrays = function(arr1, arr2) {
   let result = '';
   arr1.forEach((example1) => arr2.forEach(example2 => {
-    if (example1.length > 1 && example2.length) {
-    } else if (example1 !== example2) {
+
+    if (example1 !== example2) {
       result = false;
     } else {
       result = true;
     }
   }));
-  console.log(result);
-  //return true outcome for the assertEqual function
-  return arr1, arr2 = result
+  return arr1, arr2 = result;
 };
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => false
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS, true
