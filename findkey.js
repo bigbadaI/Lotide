c1ff7360678327cb82efe330bccfe2c834524cd5
BigBadAI <flashmefusion@hotmail.ca>
2021-01-08 01:05:09 +0000

    a function to run through an array and use a callback to find a key in the nested object and return the origin key

---
+++ findkey.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚫🚫🚫 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(obj, callback) {
+  for (let key of Object.keys(obj)) {
+
+    if (callback(obj[key])) {
+      return key;
+    }
+  }
+};
+
+
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma"); // => "noma"
