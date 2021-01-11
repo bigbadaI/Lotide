6b2cd2aa95304d4094f14b91f9463bc873200a85
BigBadAI <flashmefusion@hotmail.ca>
2021-01-11 21:24:02 +0000

    upated middleTest and tailTest to utilize mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,39 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+// const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
 
-assertArraysEqual(middle([1, 2]));
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3]); // => [3, 4]
-assertArraysEqual(middle(['Alex', "cool", 'dogs', 'beer']), ["cool", 'dogs']);
+// assertArraysEqual(middle([1, 2]));
+// assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3]); // => [3, 4]
+// assertArraysEqual(middle(['Alex', "cool", 'dogs', 'beer']), ["cool", 'dogs']);
+
+describe('#middle', () => {
+  it("should return empty array if array length is <= 2", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  it("should return [2] for the array [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3,]), [2]);
+  });
+  it("should return [2, 3] for the array [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it("should return error for comparing [3] to [1, 2, 3, 4, 5, 6]", () => {
+    assert.notDeepEqual(middle([1, 2, 3, 4, 5, 6]), [3]);
+  });
+  it("should return ['cool', 'dogs',] for the array ['Alex', 'cool', 'dogs', 'beer']", () => {
+    assert.deepEqual(middle(['Alex', "cool", 'dogs', 'beer']), ["cool", 'dogs']);
+  });
+
+});
+
+
+
+
+
+
+
+
+
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,23 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
 
-const words = ["Yo Yo", "back", "Dog"];
-console.log(tail(words)); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-console.log(words);
+// const words = ["Yo Yo", "back", "Dog"];
+// console.log(tail(words)); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
+// console.log(words);
+
+describe("#tail", () => {
+
+  it("return [6, 7] for [5, 6, 7]", () => {
+    assert.deepEqual(tail([5, 6, 7]), [6, 7]);
+  });
+
+  it("return an error if items do not match like 2 for [5, 7, 8]", () => {
+    assert.notDeepEqual(tail([5, 7, 8]), [2]);
+  });
+
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+});
