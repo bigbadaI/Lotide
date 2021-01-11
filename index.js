e54c4a61f2444f5bface35672b95d38a2ff8d236
BigBadAI <flashmefusion@hotmail.ca>
2021-01-11 21:57:23 +0000

    removed the assert functions because they print to the console and not nessecary with mocha and chai running

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const head = require('./head');
 const tail = require('./tail');
 const middle = require('./middle');
-const assertArraysEqual = require('./assertArraysEqual');
-const assertEqual = require('./assertEqual');
+
 const eqArrays = require('./eqArrays');
 
 
@@ -12,7 +11,5 @@ module.exports = {
   head,
   tail,
   middle,
-  assertArraysEqual,
-  assertEqual,
   eqArrays
 };
