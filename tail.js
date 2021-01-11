03273f75e37d7afd30c90662384b1c4589891cb2
BigBadAI <flashmefusion@hotmail.ca>
2021-01-11 16:21:47 +0000

    refactored tail.js to be called on from outside the script and moved test cases into their own file

--- tail.js
+++ tail.js
@@ -1,10 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🚫🚫🚫 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+
+
+
 const tail = function(actualArr) {
   const returnArr = [];
   if (actualArr.length < 2) {
@@ -16,7 +12,5 @@ const tail = function(actualArr) {
   }
   return returnArr;
 };
-const words = ["Yo Yo", "LightHouse","Labs"];
-console.log(tail(words)); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-console.log(words);
+
+module.exports = tail;
